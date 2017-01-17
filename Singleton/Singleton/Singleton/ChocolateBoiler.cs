70f7d86be1d743cda0426e217ff1287c53794855
Martynas Kanapinskas <martis347@gmail.com>
2017-01-17 22:19:01 +0200

    Added Singleton pattern

---
+++ Singleton/Singleton/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class ChocolateBoiler
+    {
+        private static readonly ChocolateBoiler Boilder = new ChocolateBoiler();
+        private bool _empty;
+        private bool _boiled;
+        private readonly object _emptyLock = new object();
+        private readonly object _boiledLock = new object();
+        private ChocolateBoiler()
+        {
+            _empty = true;
+            _boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            return Boilder;
+        }
+
+        public void Fill()
+        {
+            if (_empty)
+            {
+                lock (_emptyLock)
+                {
+                    if (_empty)
+                    {
+                        Console.WriteLine("Filling boiler");
+                        _empty = false;
+                    }
+                }
+            }
+        }
+
+        public void Boil()
+        {
+            if (!_empty && !_boiled)
+            {
+                lock (_emptyLock)
+                {
+                    lock (_boiledLock)
+                    {
+                        if (!_empty && !_boiled)
+                        {
+                            Console.WriteLine("Boiling boiler");
+                            _boiled = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Drain()
+        {
+            if (!_empty && _boiled)
+            {
+                lock (_emptyLock)
+                {
+                    lock (_boiledLock)
+                    {
+                        if (!_empty && _boiled)
+                        {
+                            Console.WriteLine("Draining boiler");
+                            _empty = true;
+                            _boiled = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.GetInstance();
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 50; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() => boiler.Fill()));
+                tasks.Add(Task.Factory.StartNew(() => boiler.Drain()));
+                tasks.Add(Task.Factory.StartNew(() => boiler.Boil()));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
